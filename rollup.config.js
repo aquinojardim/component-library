f0ab6b1980455083c80b7a6398035ad99b744da5
Gabriela Jardim Aquino <aquinojardim@hotmail.com>
2020-10-14 13:09:25 -0700

    delete not used dependences

--- rollup.config.js
+++ rollup.config.js
@@ -1,25 +1,24 @@
-import peerDepsExternal from "rollup-plugin-peer-deps-external";
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import typescript from "rollup-plugin-typescript2";
-import postcss from "rollup-plugin-postcss";
-import copy from "rollup-plugin-copy";
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from 'rollup-plugin-typescript2';
+import postcss from 'rollup-plugin-postcss';
 
-const packageJson = require("./package.json");
+const packageJson = require('./package.json');
 
 export default {
-  input: "src/index.ts",
+  input: 'src/index.ts',
   output: [
     {
       file: packageJson.main,
-      format: "cjs",
-      sourcemap: true
+      format: 'cjs',
+      sourcemap: true,
     },
     {
       file: packageJson.module,
-      format: "esm",
-      sourcemap: true
-    }
+      format: 'esm',
+      sourcemap: true,
+    },
   ],
   plugins: [
     peerDepsExternal(),
@@ -27,49 +26,5 @@ export default {
     commonjs(),
     typescript({ useTsconfigDeclarationDir: true }),
     postcss(),
-    copy({
-      targets: [
-        {
-          src: "src/body.scss",
-          dest: "build",
-          rename: "body.scss"
-        },
-        {
-          src: "src/breakpoint.scss",
-          dest: "build",
-          rename: "breakpoint.scss"
-        },
-        {
-          src: "src/classes.scss",
-          dest: "build",
-          rename: "classes.scss"
-        },
-        {
-          src: "src/colors.scss",
-          dest: "build",
-          rename: "colors.scss"
-        },
-        {
-          src: "src/header.scss",
-          dest: "build",
-          rename: "header.scss"
-        },
-        {
-          src: "src/padding.scss",
-          dest: "build",
-          rename: "padding.scss"
-        },
-        {
-          src: "src/tags.scss",
-          dest: "build",
-          rename: "tags.scss"
-        },
-        {
-          src: "src/typography.scss",
-          dest: "build",
-          rename: "typography.scss"
-        }
-      ]
-    })
-  ]
+  ],
 };
